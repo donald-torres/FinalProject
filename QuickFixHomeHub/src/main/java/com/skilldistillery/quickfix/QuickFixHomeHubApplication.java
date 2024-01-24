3a4830a7c6418b7673075222d63ec847a55b886c
Donald Torres <dctorres5597@gmail.com>
2024-01-24 11:28:53 -0700

    Created SpringBoot REST project and linked to JPA

---
+++ QuickFixHomeHub/src/main/java/com/skilldistillery/quickfix/QuickFixHomeHubApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.quickfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickFixHomeHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickFixHomeHubApplication.class, args);
+	}
+
+}
---
+++ QuickFixHomeHub/src/test/java/com/skilldistillery/quickfix/QuickFixHomeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.quickfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickFixHomeHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
