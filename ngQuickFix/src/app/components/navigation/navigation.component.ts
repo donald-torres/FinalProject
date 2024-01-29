d0d667aed82f414212f9160a2ddef69008501b6b
Deonnaa <aponte.deonna@gmail.com>
2024-01-29 16:45:08 -0500

    working on nav, about, contact

--- ngQuickFix/src/app/components/navigation/navigation.component.ts
+++ ngQuickFix/src/app/components/navigation/navigation.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { IndexComponent } from '../index/index.component';
 import { LoginComponent } from '../login/login.component';
 import { LogoutComponent } from '../logout/logout.component';
 import { RegisterComponent } from '../register/register.component';
 import { AuthService } from '../../services/auth.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navigation',
@@ -19,21 +20,27 @@ import { AuthService } from '../../services/auth.service';
     CommonModule,
     FormsModule,
     RouterLink,
+    NgbModule
   ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css',
 })
 export class NavigationComponent implements OnInit {
+
   account: boolean = false;
 
   public isCollapsed: boolean = false;
 
-      constructor(private authService: AuthService){}
-      ngOnInit(): void { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ){}
 
-      loggedIn(){
-         this.account = this.authService.checkLogin();
-         return this.account;
-      }
+  ngOnInit(): void { }
 
+  loggedIn(){
+    this.account = this.authService.checkLogin();
+   return this.account;
   }
+
+}
